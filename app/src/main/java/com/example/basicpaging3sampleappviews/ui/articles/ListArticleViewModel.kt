c3183bc4c49c184602b09074f6af06f9a6029356
HamidReza Farahani <h.farahani>
2023-06-17 12:28:06 +0430

    modified view model

--- app/src/main/java/com/example/basicpaging3sampleappviews/ui/articles/ListArticleViewModel.kt
+++ app/src/main/java/com/example/basicpaging3sampleappviews/ui/articles/ListArticleViewModel.kt
@@ -6,15 +6,10 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import androidx.paging.filter
-import androidx.paging.map
 import com.example.basicpaging3sampleappviews.data.Article
-import com.example.basicpaging3sampleappviews.data.repo.Repository
 import com.example.basicpaging3sampleappviews.domain.GetArticlesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.cache
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 private const val ITEMS_PER_PAGE = 50
@@ -26,10 +21,13 @@ class ListArticleViewModel @Inject constructor(
 
     val items: Flow<PagingData<Article>>
         get() {
-            val config = PagingConfig(pageSize = ITEMS_PER_PAGE, enablePlaceholders = false)
-            val pagingSourceFactory = { articlesUseCase() }
-            return Pager(config) {
-                pagingSourceFactory()
+            return Pager(
+                config = PagingConfig(
+                    pageSize = ITEMS_PER_PAGE,
+                    enablePlaceholders = false
+                )
+            ) {
+                articlesUseCase()
             }.flow.cachedIn(viewModelScope)
         }
 }
