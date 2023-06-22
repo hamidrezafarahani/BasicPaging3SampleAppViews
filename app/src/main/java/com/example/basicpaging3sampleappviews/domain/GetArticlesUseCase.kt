9d62e6b35c8eb43ba3ea793b4bd316bf3151408f
HamidReza Farahani <h.farahani>
2023-06-22 19:26:20 +0430

    modified domain layer and view model

--- app/src/main/java/com/example/basicpaging3sampleappviews/domain/GetArticlesUseCase.kt
+++ app/src/main/java/com/example/basicpaging3sampleappviews/domain/GetArticlesUseCase.kt
@@ -1,15 +1,27 @@
 package com.example.basicpaging3sampleappviews.domain
 
-import androidx.paging.PagingSource
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.example.basicpaging3sampleappviews.data.Article
 import com.example.basicpaging3sampleappviews.data.repo.Repository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
+
+private const val ITEMS_PER_PAGE = 50
 
 class GetArticlesUseCase @Inject constructor(
     private val repository: Repository
 ) {
 
-    operator fun invoke(): PagingSource<Int, Article> {
-        return repository.articlePagingSource()
-    }
+    operator fun invoke(): Flow<PagingData<Article>> = Pager(
+        config = PagingConfig(
+            pageSize = ITEMS_PER_PAGE,
+            enablePlaceholders = false
+        )
+    ) {
+        repository.articlePagingSource()
+    }.flow.flowOn(Dispatchers.IO)
 }
--- app/src/main/java/com/example/basicpaging3sampleappviews/ui/articles/ListArticleViewModel.kt
+++ app/src/main/java/com/example/basicpaging3sampleappviews/ui/articles/ListArticleViewModel.kt
@@ -2,8 +2,6 @@ package com.example.basicpaging3sampleappviews.ui.articles
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.example.basicpaging3sampleappviews.data.Article
@@ -12,22 +10,10 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-private const val ITEMS_PER_PAGE = 50
-
 @HiltViewModel
 class ListArticleViewModel @Inject constructor(
     private val articlesUseCase: GetArticlesUseCase
 ) : ViewModel() {
 
-    val items: Flow<PagingData<Article>>
-        get() {
-            return Pager(
-                config = PagingConfig(
-                    pageSize = ITEMS_PER_PAGE,
-                    enablePlaceholders = false
-                )
-            ) {
-                articlesUseCase()
-            }.flow.cachedIn(viewModelScope)
-        }
+    val items: Flow<PagingData<Article>> get() = articlesUseCase().cachedIn(viewModelScope)
 }
