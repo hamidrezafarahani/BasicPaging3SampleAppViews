b864cce4ae5bb910629990f7c843939164e2aab6
HamidReza Farahani <h.farahani>
2023-06-23 17:46:05 +0430

    remove unnecessary codes

--- app/src/main/java/com/example/basicpaging3sampleappviews/ui/articles/ListArticleFragment.kt
+++ app/src/main/java/com/example/basicpaging3sampleappviews/ui/articles/ListArticleFragment.kt
@@ -50,12 +50,6 @@ class ListArticleFragment : Fragment(R.layout.fragment_list_article) {
     private fun setupObservers() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                /*viewModel.items.collectLatest {
-                    adapter.submitData(it)
-                }*/
-
-                // OR USING THIS
-
                 viewModel.articles.collectLatest {
                     adapter.submitData(it)
                 }
--- app/src/main/java/com/example/basicpaging3sampleappviews/ui/articles/ListArticleViewModel.kt
+++ app/src/main/java/com/example/basicpaging3sampleappviews/ui/articles/ListArticleViewModel.kt
@@ -3,14 +3,10 @@ package com.example.basicpaging3sampleappviews.ui.articles
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
-import androidx.paging.cachedIn
 import com.example.basicpaging3sampleappviews.data.Article
 import com.example.basicpaging3sampleappviews.domain.GetArticlesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.shareIn
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -18,17 +14,6 @@ import javax.inject.Inject
 class ListArticleViewModel @Inject constructor(
     private val articlesUseCase: GetArticlesUseCase
 ) : ViewModel() {
-
-    val items: SharedFlow<PagingData<Article>>
-        get() = articlesUseCase().cachedIn(viewModelScope).catch {
-            Timber.tag(this@ListArticleViewModel::class.java.simpleName).d(it)
-        }.shareIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(),
-            replay = 1
-        )
-
-    // OR USING THIS
 
     val articles: SharedFlow<PagingData<Article>>
         get() = articlesUseCase.items(viewModelScope) {
